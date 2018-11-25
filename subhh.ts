04795075a9f5f4b8d4790a31541c6ef0aa37d659
Sven-S. Porst <ssp-web@earthlingsoft.net>
2018-11-25 22:41:19 +0000

    add basic facet configuration

--- subhh.ts
+++ subhh.ts
@@ -5,5 +5,54 @@ plugin.tx_find {
             port = 8983
             path = /solr/hos
         }
+
+        facets {
+            10 {
+                id = source
+            }
+
+            20 {
+                id = openaccess
+                field = rightsOA
+            }
+
+            30 {
+                id = year
+                field = publicationYear
+            }
+
+            40 {
+                id = map
+                field = geoLocationPoint
+            }
+
+            50 {
+                id = publisher
+                field = publisher_facet
+            }
+
+            60 {
+                id = subject
+            }
+
+            70 {
+                id = ddc
+                field = subject_ddc
+            }
+
+            80 {
+                id = creator
+                field = creatorName_facet
+            }
+
+            90 {
+                id = type
+                field = resourceType
+            }
+
+            100 {
+                id = language
+            }
+        }
     }
 }
