c9f8137faaca8f567051b407e54e02054c37832c
Sven-S. Porst <ssp-web@earthlingsoft.net>
2019-04-07 23:59:03 +0200

    TS / Template to explore Fluid issues when removing parameters with »,« in their name

---
+++ remove-comma/remove-comma.ts
@@ -0,0 +1,13 @@
+config.contentObjectExceptionHandler = 0
+
+#
+# Example configuration to demonstrate an Issue with f:link.action.
+#
+
+plugin.tx_find {
+    view {
+        templateRootPaths {
+            20 = typo3conf/find/remove-comma/Templates
+        }
+    }
+}
